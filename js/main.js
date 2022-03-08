1f0a1f9eff978d002d8a2761fd5a7a26b11575b4
dkayabe <dkayabe@users.noreply.github.com>
2022-03-07 22:53:20 -0600

    3/07/2022
    updated main.js

--- js/main.js
+++ js/main.js
@@ -9,27 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
   let guessedWordCount = 0;
 
   const keys = document.querySelectorAll(".keyboard-row button");
+  
+  var list = ["troll"]
 
   function getNewWord() {
-    fetch(
-      `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-          "x-rapidapi-key": "<YOUR_KEY_GOES_HERE>",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((res) => {
-        word = res.word;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    return list.random();
   }
 
   function getCurrentWordArr() {
@@ -84,6 +68,8 @@ document.addEventListener("DOMContentLoaded", () => {
     })
       .then((res) => {
         if (!res.ok) {
+			//if not in our array, throw array
+			
           throw Error();
         }
 
